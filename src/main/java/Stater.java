044ad483b4dd61f6661332f8e0d06efaa396d7e1
Yashan97 <s.d.y.anupama@gmail.com>
2026-01-01 12:13:24 +0530

    project initial commit

---
+++ src/main/java/Stater.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Stater extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/mainFxmlForm.fxml"))));
+        stage.setTitle("EWords Application");
+        stage.show();
+    }
+}
