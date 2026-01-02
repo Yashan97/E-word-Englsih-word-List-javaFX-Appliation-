709c1bddae88680c012f515b5a33f7f8f619c499
Yashan97 <s.d.y.anupama@gmail.com>
2026-01-02 12:56:56 +0530

    connecting JDBC

--- src/main/java/controller/MainFormControlelr.java
+++ src/main/java/controller/MainFormControlelr.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainFormControlelr {
+
+
+
+
+    private void loadTable(){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/english_words1", "root", "1234");
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnSearchTop(ActionEvent actionEvent) {
+        loadTable();
+    }
 }
+
