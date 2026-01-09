73e030936e0491b53e1b32d6fff52a136a6be28e
Yashan97 <s.d.y.anupama@gmail.com>
2026-01-09 13:53:49 +0530

    create search word from database

--- src/main/java/controller/AddFormController.java
+++ src/main/java/controller/AddFormController.java
@@ -7,20 +7,19 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AddFormController {
 
     public TextField txtNewWord;
     public TextField txtMeaningOfWord;
     public TextField txtSinhalaMean;
+    public Label lblAvailable;
 
     public void switchWindow(ActionEvent event, String fxmlPath, String title) throws IOException {
 
@@ -67,5 +66,25 @@ public class AddFormController {
 
     public void btnView(ActionEvent event) throws IOException {
         switchWindow(event,"/view/viewAllWords.fxml", "View All Word");
+    }
+
+    public void btnSearchQuic(ActionEvent event) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement psTm = connection.prepareStatement("SELECT * from wordlist WHERE new_word LIKE ?");
+        psTm.setString(1,"%"+txtNewWord.getText()+"%");
+        ResultSet resultSet = psTm.executeQuery();
+        if (resultSet.next()){
+            lblAvailable.setText("Available");
+            txtMeaningOfWord.setText(resultSet.getString("meaningOfWord"));
+            txtSinhalaMean.setText(resultSet.getString("sinhala_Meaning"));
+
+        }else {
+            lblAvailable.setText("Not Available");
+            txtSinhalaMean.setText("");
+            txtMeaningOfWord.setText("");
+        }
+        resultSet.close();
+        psTm.close();
+        connection.close();
     }
 }
